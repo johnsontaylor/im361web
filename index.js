d674ec542543600c9e8645d9a9771d8bb8ac9e9e
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-02-03 18:01:50 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ for (var i=1; i < 101; i++){
     else if (i % 3 == 0) console.log("Fizz");
     else if (i % 5 == 0) console.log("Buzz");
     else console.log(i);
-}
+} //https://codeburst.io/javascript-breaking-down-the-shortest-possible-fizzbuzz-answer-94a0ad9d128a
