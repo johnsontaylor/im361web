16d2017eb51c24a1dde2f7c5701a7955efefa65d
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-02-03 18:09:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 for (var i=1; i < 101; i++){
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
+    if (i % 15 == 0) console.log("SheLookedSurprised");
+    else if (i % 3 == 0) console.log("IToldMyWifeSheWasDrawing");
+    else if (i % 5 == 0) console.log("HerEyebrowsTooHigh");
     else console.log(i);
 } //https://codeburst.io/javascript-breaking-down-the-shortest-possible-fizzbuzz-answer-94a0ad9d128a
