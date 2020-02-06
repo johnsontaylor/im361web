2d0f721fce654b4fb53de0d611f646f2ddaebd10
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-02-05 18:37:45 -0600

    Intro to Web Frameworks

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import Vue from 'vue/dist/vue.js'
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Welcome to Chop Chop!'
+    message: 'Welcome to Chop Chop!',
+    counter: 0
   }
 });
 
