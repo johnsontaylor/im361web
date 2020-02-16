33602713ef4ace3320b57672fee9d658eb5c17a7
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-02-16 13:45:39 -0600

    CSS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Welcome to Chop Chop!',
+    message: 'Chop Chop',
     counter: 0,
     userName: "User: Taylor",
     options: "What food category are you interested in?",
