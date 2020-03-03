2be9ee3dd4c03b7180d9a737e14c6f09ffab5e01
johnsontaylor <60371093+johnsontaylor@users.noreply.github.com>
2020-03-02 18:50:38 -0600

    Button Work

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ var app = new Vue({
   el: '#app',
   data: {
     pageName: "home",
-    message: 'Chop Chop',
+    message: '',
     counter: 0,
     userName: "User: Taylor",
     options: "What food category are you interested in?",
@@ -15,11 +15,37 @@ var app = new Vue({
     cart: [],
     complaints: [],
     vuedropdown: [],
+    itemsUserCanOrder: [
+      {
+        restaurant: "McDonalds",
+        foodType: "Chicken",
+        size: "10 Piece",
+        deliveryService: "Doordash",
+        price: 8,
+        deliveryTime: 5,
+        image: "images/chickenNuggetsImage.jpg"
+      },
+      {
+        restaurant: "Domino's",
+        foodType: "Pizza",
+        size: "Large Cheese",
+        deliveryService: "Uber Eats",
+        price: 11,
+        deliveryTime: 15,
+        image: "images/burgerImage.jpg"
+      }
+    ],
     },
     methods: {
         makeActive: function(item){
             this.active = item;
         },
+        clearCart: function(){
+          this.cart = [];
+        },
+        addToMyPlate: function(item){
+          this.cart.push (item);
+        }
     }
 });
 
